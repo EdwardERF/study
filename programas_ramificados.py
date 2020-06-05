250c9a2e435a0127819ae84d766053aa3e03b569
Edward Rodriguez <edwarderf@gmail.com>
2020-06-04 22:23:05 -0300

    Programas ramificados - condicional IF

---
+++ programas_ramificados.py
@@ -0,0 +1,14 @@
+# las ramificaciones son codigo que se ejecuta segun condicionales
+
+# Ejemplo:
+# if <condition>:
+#     <code>
+
+number = int(input('Inserta un numero: '))
+
+if (number > 5):
+    print('El numero es mayor a 5')
+elif (number < 5):
+    print('El numero es menor a 5')
+else:
+    print('El numero es 5')
