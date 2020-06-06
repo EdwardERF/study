6f1d0ef4f6d4552cb28ed069849b52132cd42185
Edward Rodriguez <edwarderf@gmail.com>
2020-06-06 00:30:56 -0300

    Algoritmo - Busqueda binaria - Busqueda super eficiente

--- aproximacion_soluciones.py
+++ aproximacion_soluciones.py
@@ -3,11 +3,20 @@
 # Se acepta un margen de error al cual se le llama EPSILON
 # EPSILON puede aceptar mayor o menor margen de error, pero esta balanza conlleva en más o menos recursos requeridos
 
+# numero al que se le calculará la raiz cuadrada
 objetivo = int(input('Escoge un numero: '))
+
+# margen de error para encontrar esa raiz cuadrada
 epsilon = 0.01
+
+# valor que se irá sumando secuencialmente hasta encontrar la raiz cuadrada
 paso = epsilon**2
+
+# se comenzará a buscar a la raiz cuadrada desde 0.0 en adelante
 respuesta = 0.0
 
+# mientras que respuesta al cuadrado no sea igual al objetivo (con un margen de error de 0.01, es decir epsilon), este while se seguirá ejecutando
+# respuesta <= objetivo: codigo defensivo; si respuesta es mayor a objetivo, el while seguirá infinitamente, y nunca encontrará a la raíz cuadrada (la raíz cuadrada nunca sería mas grande que el objetivo)
 while abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:
     respuesta += paso
 
