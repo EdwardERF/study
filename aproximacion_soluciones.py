72c473641321a74477f6fbaf4e05205fa03effd5
Edward <edwarderf@gmail.com>
2020-06-07 18:36:38 -0300

    Ejercicio de clase de 16 - Agrupar algoritmos en Funciones y llamarlas

--- aproximacion_soluciones.py
+++ aproximacion_soluciones.py
@@ -3,24 +3,23 @@
 # Se acepta un margen de error al cual se le llama EPSILON
 # EPSILON puede aceptar mayor o menor margen de error, pero esta balanza conlleva en más o menos recursos requeridos
 
-# numero al que se le calculará la raiz cuadrada
-objetivo = int(input('Escoge un numero: '))
+def aproximacion_soluciones(objetivo):
 
-# margen de error para encontrar esa raiz cuadrada
-epsilon = 0.01
+    # margen de error para encontrar esa raiz cuadrada
+    epsilon = 0.01
 
-# valor que se irá sumando secuencialmente hasta encontrar la raiz cuadrada
-paso = epsilon**2
+    # valor que se irá sumando secuencialmente hasta encontrar la raiz cuadrada
+    paso = epsilon**2
 
-# se comenzará a buscar a la raiz cuadrada desde 0.0 en adelante
-respuesta = 0.0
+    # se comenzará a buscar a la raiz cuadrada desde 0.0 en adelante
+    respuesta = 0.0
 
-# mientras que respuesta al cuadrado no sea igual al objetivo (con un margen de error de 0.01, es decir epsilon), este while se seguirá ejecutando
-# respuesta <= objetivo: codigo defensivo; si respuesta es mayor a objetivo, el while seguirá infinitamente, y nunca encontrará a la raíz cuadrada (la raíz cuadrada nunca sería mas grande que el objetivo)
-while abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:
-    respuesta += paso
+    # mientras que respuesta al cuadrado no sea igual al objetivo (con un margen de error de 0.01, es decir epsilon), este while se seguirá ejecutando
+    # respuesta <= objetivo: codigo defensivo; si respuesta es mayor a objetivo, el while seguirá infinitamente, y nunca encontrará a la raíz cuadrada (la raíz cuadrada nunca sería mas grande que el objetivo)
+    while abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:
+        respuesta += paso
 
-if abs(respuesta**2 - objetivo) >= epsilon:
-    print(f'No se encontro la raiz cuadrada de {objetivo}')
-else:
-    print(f'La raiz cuadrada de {objetivo} es {respuesta}')
+    if abs(respuesta**2 - objetivo) >= epsilon:
+        print(f'No se encontro la raiz cuadrada de {objetivo}')
+    else:
+        print(f'La raiz cuadrada de {objetivo} es {respuesta}')
--- enumeracion_exhaustiva.py
+++ enumeracion_exhaustiva.py
@@ -2,14 +2,13 @@
 # 'adivina y verifica'
 # no es el algoritmo más eficiente, pero es muy eficaz, y las computadoras de hoy en día son muy potentes, que no es complicación utilizarlo.
 
-objetivo = int(input('Escoge un entero: '))
-respuesta = 0
+def enumeracion_exhaustiva(objetivo):
+    respuesta = 0
 
-while respuesta**2 > objetivo:
-    respuesta += 1
+    while respuesta**2 < objetivo:
+        respuesta += 1
 
-if respuesta**2 == objetivo:
-    print(f'La raíz cuadrada de {objetivo} es {respuesta}')
-else:
-    print(f'{objetivo} no tiene una raíz cuadrada exacta')
-
+    if respuesta**2 == objetivo:
+        print(f'La raíz cuadrada de {objetivo} es {respuesta}')
+    else:
+        print(f'{objetivo} no tiene una raíz cuadrada exacta')
